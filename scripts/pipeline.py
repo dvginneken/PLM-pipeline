a0da25c65560e30edd412f5f87bcf2e57bd21b2d
Daphne van Ginneken <d.van.ginneken@hotmail.com>
2025-08-14 11:05:24 +0200

    Ablang2 update

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -79,8 +79,54 @@ else:
     print("model_name is unknown.")
     sys.exit()
 
-#### Ablang and Sapiens have different models for heavy and light chains
-if model_name in ["Ablang","Sapiens"]:
+if model_name == "Ablang2": # Ablang2 can pair the heavy and light chains for the same sequence identifier
+    if "chain" not in sequence_file.columns:
+        print("Column 'chain' not found in input CSV. When running Ablang2, a column named 'chain' should be present to indicate heavy or light chain.")
+        sys.exit()
+
+    #Pair heavy and light chain sequences based on similar sequence id
+    paired_sequences = sequence_file[sequence_file["chain"] == "IGH"].merge(sequence_file[sequence_file["chain"] != "IGH"], on=seq_id_column, suffixes=("_HC", "_LC"))
+    paired_sequences["sequence"] = paired_sequences[sequences_column + "_HC"] + "|" + paired_sequences[sequences_column + "_LC"]
+
+    #### Perform calculations
+    
+    #Calculate pseudolikelihood, add to sequence_file, and save as CSV
+    if "pseudolikelihood" in calc_list:
+        paired_sequences["evo_likelihood"] = model.calc_pseudo_likelihood_sequence(list(paired_sequences["sequence"]))
+        paired_sequences.to_csv(os.path.join(save_path,f"evo_likelihood_{model_name}.csv"), index=False)
+
+    #Calculate probability matrices and potentially suggested mutations
+    if "probability_matrix" in calc_list:   
+        # If "suggest_mutations" is in the calc_list, create a dataframe to store all the mutations of all sequences
+        if("suggest_mutations" in calc_list):
+            all_mutations_df = pd.DataFrame()
+
+        # Calculate probability matrix (and mutations) for each sequence
+        for index in paired_sequences.index:  
+            # Calculates and saves the probability matrix for each sequence
+            prob_matrix = model.calc_probability_matrix(paired_sequences["sequence"][index])
+            seq_id = paired_sequences[seq_id_column][index]
+            prob_matrix.to_csv(os.path.join(save_path,f"prob_matrix_seq_{seq_id}_{model_name}.csv"), index = False)
+
+            # Calculate the suggested mutations for this sequence
+            if("suggest_mutations" in calc_list):
+                mutations_df = calculate_mutations(sequences_file=paired_sequences,prob_matrix=prob_matrix,num_mutations=number_mutations,
+                                                   seq_id_column=seq_id_column,sequences_column="sequence")
+                # Concatenates the mutations obtained from the current sequence to the global DataFrame
+                all_mutations_df = pd.concat([all_mutations_df, mutations_df], ignore_index=True)
+            
+        # Saves all the mutations for all sequences
+        if("suggest_mutations" in calc_list):
+            output_file = os.path.join(save_path, f"{model_name}_{number_mutations}_mutations.csv")
+            all_mutations_df.to_csv(output_file, index=False)
+            print(f"All mutations saved to: {output_file}")
+        
+    if "embeddings" in calc_list:
+        #Calculate embeddings, add to sequence_file, and save as CSV
+        model.fit_transform(paired_sequences, layer=layer, method=method, save_path=save_path, model_name=model_name, 
+                            seq_id_column=seq_id_column, sequences_column="sequences")
+
+elif model_name in ["Ablang","Sapiens"]:  # Ablang and Sapiens have different models for heavy and light chains
     if "chain" not in sequence_file.columns:
         print("Column 'chain' not found in input CSV. When running Ablang or Sapiens, a column named 'chain' should be present to indicate heavy or light chain.")
         sys.exit()
@@ -117,13 +163,8 @@ if model_name in ["Ablang","Sapiens"]:
 
             # Calculate the suggested mutations for this sequence
             if("suggest_mutations" in calc_list):
-                mutations_df = calculate_mutations(
-                    sequences_file=sequence_file.loc[[index]],
-                    prob_matrix=prob_matrix,
-                    num_mutations=number_mutations,
-                    seq_id_column=seq_id_column,
-                    sequences_column=sequences_column
-                    )
+                mutations_df = calculate_mutations(sequences_file=sequence_file.loc[[index]],prob_matrix=prob_matrix,num_mutations=number_mutations,
+                                                   seq_id_column=seq_id_column,sequences_column=sequences_column)
                 # Concatenates the mutations obtained from the current sequence to the global DataFrame
                 all_mutations_df = pd.concat([all_mutations_df, mutations_df], ignore_index=True)
             
@@ -178,13 +219,9 @@ else: #If model is not Ablang or Sapiens:
             
             # Calculate the suggested mutations for this sequence
             if("suggest_mutations" in calc_list):
-                mutations_df = calculate_mutations(
-                    sequences_file=sequence_file.loc[[index]],
-                    prob_matrix=prob_matrix,
-                    num_mutations=number_mutations,
-                    seq_id_column=seq_id_column,
-                    sequences_column=sequences_column
-                    )
+                mutations_df = calculate_mutations(sequences_file=sequence_file.loc[[index]], prob_matrix=prob_matrix,
+                                                    num_mutations=number_mutations, seq_id_column=seq_id_column,
+                                                    sequences_column=sequences_column)
                 # Concatenates the mutations obtained from the current sequence to the global DataFrame
                 all_mutations_df = pd.concat([all_mutations_df, mutations_df], ignore_index=True)
             
